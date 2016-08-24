b236fc9959af633cf8ce6b957eb9667058193e6f
Alex <aartsiomenka@gsu.edu>
2016-08-24 11:29:15 -0400

    Examples from lecture 08/23

---
+++ Example1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+
+            a.RaiseEvent();
+
+            a.Event += SayHi;
+            a.RaiseEvent();
+            a.Event -= SayHi;
+            a.RaiseEvent();
+
+            
+        }
+
+        static string SayHi()
+        {
+            Console.WriteLine("Hi");
+            return "Hi";
+        }
+    }
+
+    class A
+    {
+
+        public event Func<string> Event;
+
+        public void RaiseEvent()
+        {
+            if (Event != null)
+                Event();
+        }
+    }
+}
