eb2206d820127cab045088b6dd1a717ffb18c42e
Alex <aartsiomenka@gsu.edu>
2016-08-30 11:56:38 -0400

    Lecture 8/30

---
+++ Example4/Example4/AnotherWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Example4
+{
+    /// <summary>
+    /// Interaction logic for AnotherWindow.xaml
+    /// </summary>
+    public partial class AnotherWindow : Window
+    {
+        public bool IsCancel = false;
+        public AnotherWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_no_OnClick(object sender, RoutedEventArgs e)
+        {
+            IsCancel = true;
+            Close();
+        }
+
+        private void Button_yes_OnClick(object sender, RoutedEventArgs e)
+        {
+            IsCancel = false;
+            Close();
+        }
+    }
+}
