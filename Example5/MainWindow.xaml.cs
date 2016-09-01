210f1cf6b33456d980bb8cca88b2d7e038ef00cf
Alex <aartsiomenka@gsu.edu>
2016-09-01 11:45:50 -0400

    Lecture 9/1

--- Example5/MainWindow.xaml.cs
+++ Example5/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace Example5
 {
@@ -21,7 +24,7 @@ namespace Example5
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly List<Button> _buttons = new List<Button>();
+        private List<Button> _buttons = new List<Button>();
         private Button _empty;
         private const int SIZE = 4;
         private const int SHUFFLE_DEPTH = 1000;
@@ -71,8 +74,42 @@ namespace Example5
             for (int i = 0; i < SHUFFLE_DEPTH; ++i)
             {
                 int j = rnd.Next(_buttons.Count);
-                ButtonClick(_buttons[j], null);
+                
+                _buttons[j].RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
             }
+        }
+
+        class ButtonCmp : IComparer<Button>
+        {
+            private Button _empty;
+            public ButtonCmp(Button empty)
+            {
+                _empty = empty;
+            }
+            public int Compare(Button x, Button y)
+            {
+                if (x == _empty) return 1;
+                if (y == _empty) return -1;
+                int ix = int.Parse(x.Content.ToString());
+                int iy = int.Parse(y.Content.ToString());
+                return ix.CompareTo(iy);
+            }
+        }
+
+        private void ResetPane(object sender, RoutedEventArgs e)
+        {
+            
+            _buttons.Sort(new ButtonCmp(_empty));
+//            _buttons = _buttons.OrderBy(
+//                b => b == _empty
+//                    ? int.MaxValue
+//                    : int.Parse(b.Content.ToString())).ToList();
+            FillGrid();
+        }
+
+        private void MenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
