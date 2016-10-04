78bbae0b92cc7a8f50a51e1c4f75c4ccba562f95
Alex <aartsiomenka@gsu.edu>
2016-10-04 11:42:22 -0400

    Lecture 10/4

--- Example10/View/MainWindow.xaml.cs
+++ Example10/View/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Example10.ViewModel;
 
 namespace Example10
 {
@@ -23,11 +25,47 @@ namespace Example10
         public MainWindow()
         {
             InitializeComponent();
+            ViewModel.PropertyChanged += OnViewModelOnPropertyChanged;
+            Types.ItemsSource = ViewModel.Types;
+        }
+
+        private void OnViewModelOnPropertyChanged(object o, PropertyChangedEventArgs args)
+        {
+            switch (args.PropertyName)
+            {
+                case "Parameter":
+                    Type.Content = ViewModel.Parameter;
+                    break;
+                case "GeneratedValue":
+                    GeneratedValue.Text = ViewModel.GeneratedValue;
+                    break;
+                case "SelectedIndex":
+                    Types.SelectedIndex = ViewModel.SelectedIndex;
+                    break;
+                case "Length":
+                    Length.Text = ViewModel.Length.ToString();
+                    break;
+            }
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
             ViewModel.Update();
+        }
+
+        private void Types_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ViewModel.SelectedIndex = Types.SelectedIndex;
+        }
+
+        private void Length_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var l = Length.Text;
+            var len = 0;
+            if (GeneratedValue != null && int.TryParse(l, out len))
+            {
+                ViewModel.Length = len;
+            }
         }
     }
 }
