f305e64e0c5b13ee0740dbd3dc4380a09a54e2c4
night-stalker <aartyomenko@cs.gsu.edu>
2016-10-13 01:21:40 -0400

    Pre Lecture 10/13

---
+++ Example11/View/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Example11.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RangeBase_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (Math.Abs(e.NewValue - 100) < 0.1)
+            {
+                ProgressBar.Visibility = Visibility.Hidden;
+                MyLabel.Content = "Completed!";
+            }
+        }
+    }
+}
---
+++ Example11/ViewModel/ProcessViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Example11.Annotations;
+
+namespace Example11.ViewModel
+{
+    class ProcessViewModel : INotifyPropertyChanged
+    {
+        private int _progress = 0;
+        private Random _rnd = new Random();
+
+        public ProcessViewModel()
+        {
+            Task.Run(() =>
+            {
+                while (_progress < 100)
+                {
+                    Thread.Sleep(_rnd.Next(2000));
+                    _progress += _rnd.Next(30);
+                    OnPropertyChanged("Progress");
+                } 
+            });
+        }
+        
+        public int Progress
+        {
+            get { return _progress; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
