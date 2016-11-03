703357ab324e623d0df78370cefac533d7e91dcd
night-stalker <aartyomenko@cs.gsu.edu>
2016-11-03 01:22:47 -0400

    Pre Lecture 11/3

--- Example12/MainWindow.xaml.cs
+++ Example12/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -33,16 +34,20 @@ namespace Example12
             switch (e.Key)
             {
                 case Key.Return:
-                    var m = running_text.Margin;
-                    m.Left  = 300.0;
-                    running_text.Content = text.Text;
-                    while (m.Left > -running_text.Width)
-                    {
-                        Thread.Sleep((int)Math.Round(1 * speed.Value));
-                        m.Left -= 5;
-                        running_text.Margin = m;
-                        Dispatcher.Invoke(DispatcherPriority.Render, EmptyDelegate);
-                    }
+                    //Canvas.SetLeft(running_text, 300);
+                    //running_text.Content = text.Text;
+                    //while (Canvas.GetLeft(running_text) > -running_text.ActualWidth)
+                    //{
+                    //    var l = Canvas.GetLeft(running_text);
+                    //    Thread.Sleep((int)Math.Round(1 * speed.Value));
+                    //    Canvas.SetLeft(running_text, l-1);
+                    //    Dispatcher.Invoke(DispatcherPriority.Render, EmptyDelegate);
+                    //}
+                    var sb = FindResource("anim") as Storyboard;
+                    var an = sb.Children.First() as DoubleAnimation;
+                    an.To = -running_text.ActualWidth;
+                    an.Duration = new Duration(new TimeSpan(0, 0, (int)speed.Value));
+                    sb.Begin(this);
 
                     break;
             }
